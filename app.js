24589f8cfc87c6ad40f29b5d2abe1d82577acf31
LucasrsRodrigues <lucasrs.rodrigues3@outlook.com>
2020-07-21 12:03:39 -0300

    git

---
+++ app.js
@@ -0,0 +1,60 @@
+const puppeter = require('puppeteer');
+
+// Ler a pagina do insta
+async function start(){
+
+     async function loadMore(page, selector){
+          const moreButton = await page.$(selector)
+          if(moreButton){
+               console.log('more');
+              await moreButton.click();
+              await page.waitFor(selector, {timeout: 3000}).catch(()=>{console.log('deu o tempo')});
+              await loadMore(page,selector);
+          }
+     }
+     // Pegar o Comentarios / @
+     async function getComments(page, selector){
+          const comments = await page.$$eval(selector, links => links.map(link => link.innerText));
+          return comments;
+     }
+     
+     const browser = await puppeter.launch();
+     const page = await browser.newPage();
+     await page.goto('https://www.instagram.com/p/CChMVvQgYKK/');
+
+     await loadMore(page, '.dCJp8');
+     const arrobas = await getComments(page, '.C4VMK span a');
+     const counted = count(arrobas);
+     const sorted = sort(counted);
+
+     sorted.forEach(arroba=>{console.log(arroba)});
+
+
+     await browser.close();    
+
+
+}
+
+
+
+
+// Contar arrobas repetidas
+
+function count(arrobas){
+     const count = {}
+     arrobas.forEach(arroba => {count[arroba] =  (count[arroba]||0) + 1});
+     return count
+}
+
+//console.log(count(fakeArrobas));
+// Ordenar
+
+function sort(counted){
+     const entries = Object.entries(counted);
+     const sorted = entries.sort((a,b) => (b[1] - a[1]));
+
+     return sorted;
+}
+
+//sort(count(fakeArrobas));
+start();
